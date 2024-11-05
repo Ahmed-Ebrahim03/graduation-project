8adb463889868bc0d7f23dbb4c59bc61d47b5bd0
Aly Hussein <111089237+alyHusseinn@users.noreply.github.com>
2024-11-05 23:44:56 +0200

    update(backend): register user route

--- backend/README.md
+++ backend/README.md
@@ -1,2 +1,16 @@
 # backend
-useing node.js
+
+# routes
+1. Auth
+    - POST: /api/auth/signup
+    - POST: /api/auth/login
+    - GET:  /api/auth/logout
+    - GET:  /api/auth/me
+2. summarize
+    - POST: /api/summarize
+        - post a book pdf/txt file in order to get a summary
+    - GET:  /api/summarize
+        - get a list of all summaries
+    - GET:  /api/summarize/:id
+        - get a summary by id
+    - 
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,39 @@
 import express from "express";
+import 'dotenv/config'
+import connectDB from "./config/db";
+import env from "./config/env";
+import morgan from "morgan";
+import cors from "cors";
+import userRouter from "./routes/userRouter";
 
 const app = express();
+app.use(morgan("dev"));
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
+}));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// app.use(env.API_PREFIX)
 
-app.get("/kossomk", (req, res) => {
-  res.send("555555, kossomen omk");
-});
+app.get("/hello", (req, res) => {
+  res.send("Hello World!");
+})
 
-app.listen(3000, () => {
+app.use("/api/auth", userRouter);
+// Errors handler
+app.use((error: Error, req: express.Request, res: express.Response, next: express.NextFunction) => {
+  const statusCode = res.statusCode ? res.statusCode : 500;
+  res.status(statusCode);
+  res.json({
+    message: error.message,
+    stack: process.env.NODE_ENV === "production" ? null : error.stack,
+  });
+})
+
+
+app.listen(env.PORT, async () => {
   console.log("Server is running on port 3000");
+  await connectDB();
 });
